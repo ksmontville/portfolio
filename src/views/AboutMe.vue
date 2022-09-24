37a7d35f423c66ddeffcf9dbdbb62f5fe8bad1b0
Kyle <ksmontville@gmail.com>
2022-09-23 20:08:31 -0400

    General styling.

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col flex-nowrap items-center">
-    <p class="leading-loose text-4xl">Learn about me.</p>
+  <div class="flex flex-col flex-nowrap items-center p-6">
+    <p class="leading-loose text-4xl mb-6">Learn about me.</p>
     <div class="w-full lg:w-1/3">
         <AccordionComponent />
     </div>
@@ -8,7 +8,7 @@
 
 
   <div class="flex flex-col flex-nowrap justify-center items-center p-6">
-    <p class="leading-loose text-4xl">Snippets.</p>
+    <p class="leading-loose text-4xl mb-6">Snippets.</p>
     <div class="bg-zinc-800 rounded-full p-6 w-fit lg:w-1/3">
       <CarouselComponent />
     </div>
