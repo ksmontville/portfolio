14e0a67899ab0f600365c8b4fecdd689ca77f621
Kyle <ksmontville@gmail.com>
2023-02-14 12:01:14 -0500

    Add carousel to projects page, general formatting.

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,37 +1,56 @@
 <template>
 
-  <div class="flex flex-col flex-wrap justify-center gap-8">
-    <div v-for="project in projects" :key="project.id" class="card flex flex-col lg:flex-row text-slate-200 rounded-l-lg border-2 border-red-50">
+  <div class="flex flex-col flex-wrap items-center justify-center">
+    <div class="max-w-sm min-h-full md:max-w-screen-sm lg:max-w-screen-lg xl:max-w-screen-xl p-4">
+      <Carousel :items-to-show="1" :wrap-around="true">
+        <Slide v-for="project in projects" :key="project.id">
 
-      <div class="flex-1 flex flex-col items-stretch justify-center card-img rounded-l-md border-b-8 border-r-4 border-red-50">
-        <p class="bg-red-500 rounded-l-lg text-center text-2xl border-b-8 border-red-50 p-4">{{ project.title }}</p>
-        <img class="flex-1 hover:border-4 hover:border-red-900" :src="project.image" :alt="project.alt" @click="openImage(project.image)">
-      </div>
+          <div class="card flex flex-col lg:flex-row text-slate-200 rounded-lg border-2 border-red-50">
 
-      <div class="flex-1 card-body text-slate-200 border-b-8 border-red-50 md:p-4">
-        <div class="rounded-lg p-2 flex flex-col items-center">
-          <div class="flex-1">
-            <p class="leading-loose text-center text-xl underline p-2">Guest Credentials</p>
-            <p class="leading-loose text-center text-lg mb-6"><strong>{{ project.credentials }}</strong></p>
-            <p class="leading-loose text-center text-xl underline p-2">Description</p>
-            <p class="leading-loose text-justify text-lg p-2">{{ project.description }}</p>
-            <p class="leading-loose text-center text-xl underline p-2">Technologies</p>
-            <p class="leading-loose text-left text-lg p-2">{{ project.tech }}</p>
+            <div class="flex-1 flex flex-col card-img rounded-lg rounded-r-none border-b-8 lg:border-r-4 border-red-50">
+              <div class="flex-shrink flex flex-col lg:items-start lg:justify-center gap-2 md:gap-4 lg:gap-6 p-4 md:p-6 lg:p-8 bg-red-500 border-b-4 border-red-50 rounded-lg lg:rounded-r-none rounded-b-none">
+                <p class="leading-loose text-2xl md:text-4xl">{{ project.title }}</p>
+                <p class="leading-loose md:text-xl ">{{ project.subtext }}</p>
+              </div>
+              <img class="flex-1 hover:border-4 hover:border-red-900" :src="project.image" :alt="project.alt" @click="openImage(project.image)">
+            </div>
+
+            <div class="flex-1 card-body text-slate-200 border-b-8 border-red-50 md:p-4">
+              <div class="rounded-lg p-2 flex flex-col items-center">
+                <div class="flex-1">
+                  <p class="leading-loose text-center text-xl underline p-2">Guest Credentials</p>
+                  <p class="leading-loose text-center text-lg mb-6"><strong>{{ project.credentials }}</strong></p>
+                  <p class="leading-loose text-center text-xl underline p-2">Description</p>
+                  <p class="leading-loose text-justify text-lg p-2">{{ project.description }}</p>
+                  <p class="leading-loose text-center text-xl underline p-2">Technologies</p>
+                  <p class="leading-loose text-left text-lg p-2">{{ project.tech }}</p>
+                </div>
+                <form class="mt-16 mb-8" :action="project.link" method="get" target="_blank">
+                  <button type="submit" class="rounded-md bg-red-500 text-slate-200 text-center hover:bg-red-300 p-4">Show me more!</button>
+                </form>
+              </div>
+            </div>
+
           </div>
-          <form class="mt-16 mb-8" :action="project.link" method="get" target="_blank">
-            <button type="submit" class="rounded-md bg-red-500 text-slate-200 text-center hover:bg-red-300 p-4">Show me more!</button>
-          </form>
-        </div>
-      </div>
+        </Slide>
+
+        <template #addons>
+            <Navigation />
+          </template>
+      </Carousel>
     </div>
 </div>
 </template>
 
 <script>
 import {ref} from "vue";
+import {Carousel, Navigation, Pagination, Slide} from "vue3-carousel";
 
 export default {
   name: "ProjectCard",
+  components: {
+    Carousel, Slide, Pagination, Navigation
+  },
 
   setup() {
 
@@ -39,6 +58,7 @@ export default {
       'openVegAPI': {
         'id': 1,
         'title': "OpenVeg API",
+        'subtext': "Find Vegan restaurants in your area.",
         'image': '/img/openveg-api.png',
         'alt': 'Screenshot of a prototype OpenVeg API.',
         'description': 'Prototype API for locating vegan/vegetarian restaurants by state, city, and/or zip code. Uses FastAPI' +
@@ -51,6 +71,7 @@ export default {
       'chopThatVeg': {
         'id': 2,
         'title': 'Chop That Veg!',
+        'subtext': "Instructions for chopping any vegetable!",
         'image': '/img/chop-that-veg.jpeg',
         'alt': 'Screenshot of the the Chop That Veg! API.',
         'description': 'NOTE: APP HAS CEASED FUNCTIONING SINCE HEROKU DISABLED THEIR FREE-TIER DATABASES. WORK IN PROGRESS! ' +
@@ -63,6 +84,7 @@ export default {
       'mhDashboard': {
         'id': 3,
         'title': "Mental Health Tracker",
+        'subtext': "Take control of your mental health with Dr. Liza Ignatova.",
         'image': '/img/mentalhealthdashboard.png',
         'alt': 'Screenshot of the Mental Health Dashboard webapp.',
         'description': 'A prototype web app serving as a repository of mental health resources and as a tool for' +
@@ -74,6 +96,7 @@ export default {
       'weddingRSVP': {
         'id': 4,
         'title': 'Wedding RSVP',
+        'subtext': "Open bar, great food, photography, music & dancing!",
         'image': '/img/weddingwebsite.png',
         'alt': 'Screenshot showing the wedding RSVP form and website.',
         'description': 'A personal wedding website with password protection, guest RSVP form, and links to travel accommodations which' +
@@ -96,6 +119,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style >
+
+svg.carousel__icon {
+  fill: #f87171;
+  scale: 300%;
+}
+
+@media (min-width: 768px) {
+/*  button.carousel__next {*/
+/*    left: 104%;*/
+/*  }*/
+
+/*  button.carousel__prev {*/
+/*    left: -4%;*/
+/*  }*/
+
+}
 
 </style>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,7 +1,7 @@
 <template>
   <p class="leading-loose text-center text-4xl text-zinc-200 mt-6 mb-6">Projects for Learning and Personal Use</p>
 
-  <div class="flex flex-col lg:flex-row items-center justify-center gap-y-12 gap-x-24 p-2 md:p-8">
+  <div class="flex flex-row lg:flex-row items-center justify-center md:p-8">
       <ProjectCards />
   </div>
 
